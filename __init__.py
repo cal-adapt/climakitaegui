b321f77c4d52c8d892baf8daebf9a8e853b14667
Eric Lehmer <elehmer@berkeley.edu>
2024-05-31 10:55:53 -0700

    Update inits

--- __init__.py
+++ __init__.py
@@ -1,5 +1,19 @@
 from climakitaegui.core.data_interface import Select
 
+try:
+    from importlib.metadata import version as _version
+except ImportError:
+    # if the fallback library is missing, we are doomed.
+    from importlib_metadata import version as _version  # type: ignore[no-redef]
+
+try:
+    __version__ = _version("climakitaegui")
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
+
+
 __all__ = (
     # Classes
     "Select",
