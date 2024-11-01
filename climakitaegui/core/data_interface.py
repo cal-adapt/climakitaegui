f9b45de57bdb28cd4a7a8deb82635b8868bc1c59
Eric Lehmer <elehmer@berkeley.edu>
2024-11-01 11:33:05 -0700

    try to align map left

--- climakitaegui/core/data_interface.py
+++ climakitaegui/core/data_interface.py
@@ -83,7 +83,7 @@ def _map_view(selections, stations_gdf):
     crs_proj4 = proj.proj4_init  # used below
     xy = ccrs.PlateCarree()
     ax = fig0.add_subplot(111, projection=proj)
-    mpl_pane = pn.pane.Matplotlib(fig0)
+    mpl_pane = pn.pane.Matplotlib(fig0, align='left')
 
     # Get geometry of selected location
     subarea_gpd = _get_subarea(
