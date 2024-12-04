894bbae8442e63116012ffd7916c2725eeddb9c5
Eric Lehmer <elehmer@berkeley.edu>
2024-12-04 12:38:18 -0800

    test removing one panel

--- climakitaegui/explore/warming.py
+++ climakitaegui/explore/warming.py
@@ -885,8 +885,8 @@ def warming_levels_visualize(wl_viz):
         collapsible=False,
     )
 
-    warming_panel = pn.Column(GMT_plot, map_tabs)
-    return warming_panel.show()
+    warming_panel = GMT_plot
+    return warming_panel
 
 
 def _make_hvplot(data, clabel, clim, cmap, sopt, title, width=225, height=210):
